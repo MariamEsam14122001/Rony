28ab86ac766e401f8191ab794324cb6eac65b451
MariamEsam14122001 <mariamessamadel@gmail.com>
2024-06-06 21:09:06 +0300

    udate

--- src/componets/heart/Heart.jsx
+++ src/componets/heart/Heart.jsx
@@ -1,27 +1,40 @@
 import React from "react";
-//import red from "../pictures/redhearticon.svg";
-//import empty from "../pictures/empty_heart.svg";
-//import { useWishlist } from "../Context/WishlistContext";
+import red from "../pictures/redhearticon.svg";
+import empty from "../pictures/empty_heart.svg";
+import { useWishlist } from "../Context/WishlistContext";
+
+import React from "react";
+
+import { useSelector } from "react-redux";
 
 function HeartButton({ id }) {
-  // const { wishlist, addToWishlist, removeFromWishlist } = useWishlist();
+  const { wishlist, addToWishlist, removeFromWishlist } = useWishlist();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
-  // const handleToggleLike = () => {
-  //   if (wishlist.includes(id)) {
-  //     removeFromWishlist(id);
-  //   } else {
-  //     addToWishlist(id);
-  //   }
-  // };
+  const handleToggleLike = () => {
+    if (!isAuthenticated) {
+      // Optionally, you can display a message or redirect the user to the login page
+      console.log("Please log in to add items to wishlist");
+      return;
+    }
+
+    if (wishlist.includes(id)) {
+      removeFromWishlist(id);
+    } else {
+      addToWishlist(id);
+    }
+  };
 
   return (
     <div>
-      {/*} <img
-        src={wishlist.includes(id) ? red : empty}
-        alt={wishlist.includes(id) ? 'Liked' : 'Like'}
-        onClick={handleToggleLike}
-        style={{ cursor: 'pointer' }}
-    />*/}
+      {isAuthenticated && (
+        <img
+          src={wishlist.includes(id) ? red : empty}
+          alt={wishlist.includes(id) ? "Liked" : "Like"}
+          onClick={handleToggleLike}
+          style={{ cursor: "pointer" }}
+        />
+      )}
     </div>
   );
 }
--- src/componets/item/Items.jsx
+++ src/componets/item/Items.jsx
@@ -4,19 +4,27 @@ import Item from "./Item";
 import styles from "./items.module.css";
 import { Link } from "react-router-dom";
 
-const Items = ({ accommodations }) => {
+const Items = ({ accommodations, likedItems, onToggleLike }) => {
   return (
     <div className={styles["card-container"]}>
       <div className="row  row-cols-md-3 g-3">
         {accommodations.map((accommodation) => (
-          <Link to={`/details/${accommodation.id}`}>
+          <Link
+            to={`/details/${accommodation.id}`}
+            state={{ item: accommodation }}
+            key={accommodation.id}
+          >
             <Item
               id={accommodation.id}
               title={accommodation.title}
               price={accommodation.price}
               location={accommodation.location}
-              images={accommodation.image}
+              image={accommodation.image}
+              phone={accommodation.phone}
               shared_or_individual={accommodation.shared_or_individual}
+              isLiked={likedItems.includes(accommodation.id)}
+              onToggleLike={() => onToggleLike(accommodation.id)}
+              onClick={() => handleItemClick(accommodation.id)}
             />
           </Link>
         ))}
@@ -27,27 +35,33 @@ const Items = ({ accommodations }) => {
 
 export default Items;
 
-// const Items = ({ accommodations ,likedItems, onToggleLike }) => {
+// const Items = ({ accommodations, onItemSelect, likedItems, onToggleLike }) => {
 //   return (
 //     <div className={styles["card-container"]}>
-//     <div className="row  row-cols-md-3 g-3">
-//     {accommodations.map( accommodation  => (
-// <Link to={`/details/${accommodation.id}`}>
-//           <Item
-//             key={index}
-//             id={accommodation.id}
-//             title={accommodation.title}
-//             price={accommodation.price}
-//             location={accommodation.location}
-//             image={accommodation.image}
-//shared_or_individual={accommodation.shared_or_individual}
-//             isLiked={likedItems.includes(accommodation.id)}
-//           onToggleLike={() => onToggleLike(accommodation.id)}
-//           onClick={() => handleItemClick(accommodation.id)}
-//           />
-// </Link>
-//           ))}
-//     </div>
+//       <div className="row  row-cols-md-3 g-3">
+//         {accommodations.map((accommodation) => (
+//           <Link
+//   to={`/details/${accommodation.id}`}
+//   onClick={() => onItemSelect(accommodation)}
+//   key={accommodation.id}
+// >
+//             <Item
+//               id={accommodation.id}
+//               title={accommodation.title}
+//               price={accommodation.price}
+//               location={accommodation.location}
+//               image={accommodation.image}
+//               shared_or_individual={accommodation.shared_or_individual}
+// isLiked={likedItems.includes(accommodation.id)}
+// onToggleLike={() => onToggleLike(accommodation.id)}
+// onClick={() => handleItemClick(accommodation.id)}
+
+//             />
+//           </Link>
+//         ))}
+//       </div>
 //     </div>
 //   );
 // };
+
+// export default Items;
